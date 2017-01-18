338e94da73466c62ee45048359273e0d49ddbf53
shunzl <secret466nc6@gmail.com>
2017-01-18 12:01:51 +0800

    t2

--- Tolerance.cpp
+++ Tolerance.cpp
@@ -24,12 +24,12 @@ namespace {
     public:
     VectorizeMap(): vmap() {}
     bool IsAdded(Value *sca) {
-    return vmap.find(sca)!=vmap.end();
+	return vmap.find(sca)!=vmap.end();
     }
     void AddPair(Value* sca, Value* vec) {
-    VectorizeMapTable::iterator iter=vmap.find(sca);
+	VectorizeMapTable::iterator iter=vmap.find(sca);
         if(iter == vmap.end())//do not find vectorize
-      vmap.insert(std::make_pair(sca, vec));
+	  vmap.insert(std::make_pair(sca, vec));
         else
           errs()<<"Error:"<<*sca <<" has been vectorized.\n";
     }
@@ -71,27 +71,27 @@ namespace {
         }
   }
   Value* GetVecOpValue(IRBuilder<> builder,Value* val,VectorizeMap vec_map){
-    if(isa<LoadInst>(val)){//find add inst and 2 op is load, do SIMD "add"
+	if(isa<LoadInst>(val)){//find add inst and 2 op is load, do SIMD "add"
         //errs()<< "****GetVecOpValue Load!\n";
-        LoadInst* ld_inst = cast<LoadInst>(val);//value to loadinst
-        Value* sca = ld_inst->getPointerOperand();
-        //errs()<<"sca:"<<*sca<<"\n";
-        Value *alloca_vec = vec_map.GetVector(sca);
-        //errs()<<"get vector:"<<*alloca_vec<<"\n";
-        //create load before "add"
-        LoadInst* load_val=builder.CreateLoad(alloca_vec);
-        load_val->setAlignment(16);
-        return load_val;
-    }else if(isa<BinaryOperator>(val)){
-        //errs()<< "****GetVecOpValue BinaryOperator:\n";
+		LoadInst* ld_inst = cast<LoadInst>(val);//value to loadinst
+		Value* sca = ld_inst->getPointerOperand();
+		//errs()<<"sca:"<<*sca<<"\n";
+		Value *alloca_vec = vec_map.GetVector(sca);
+		//errs()<<"get vector:"<<*alloca_vec<<"\n";
+		//create load before "add"
+		LoadInst* load_val=builder.CreateLoad(alloca_vec);
+		load_val->setAlignment(16);
+		return load_val;
+	}else if(isa<BinaryOperator>(val)){
+		//errs()<< "****GetVecOpValue BinaryOperator:\n";
         BinaryOperator* bin_inst=cast<BinaryOperator>(val);
         //errs()<<"bin_inst:"<<*bin_inst<<"\n";
         Value *bin_vec = vec_map.GetVector(bin_inst);
         //errs()<<"get vector:"<<*bin_vec<<"\n";
         return bin_vec;
-    }else if(isa<Constant>(val)){
-        //ConstantInt* c_inst=cast<ConstantInt>(val);
-        errs()<< "GetVecOpValue Constant:"<<*val <<"\n";
+	}else if(isa<Constant>(val)){
+    	//ConstantInt* c_inst=cast<ConstantInt>(val);
+		errs()<< "GetVecOpValue Constant:"<<*val <<"\n";
         //create sca alloca and vec alloca, store constant and load into insertelement.
         auto alloca_c = builder.CreateAlloca(val->getType());
         alloca_c->setAlignment(4);
@@ -110,7 +110,7 @@ namespace {
         //Value *mul_value = ConstantInt::get(val->getType() , *val); 
         for (unsigned i = 0; i < 4; i++)//for 4 copies
         {
-            val_c = builder.CreateInsertElement(val_c,load_c, builder.getInt32(i),"insertCons");  
+			val_c = builder.CreateInsertElement(val_c,load_c, builder.getInt32(i),"insertCons");  
         }
         //errs()<<"****val_c:"<<*val_c<<"\n";
         auto store_val=builder.CreateStore(val_c,allocaVec);
@@ -140,6 +140,9 @@ namespace {
       for (auto &B : F) {
        
         for (auto &I : B) {
+            errs()<<"=====";
+            I.dump();
+            errs()<<"=====";
             if (auto *op = dyn_cast<BinaryOperator>(&I)) {
                 //add into array
                 binop.push_back(op);
@@ -242,13 +245,19 @@ namespace {
       for(int i=0; i<CheckPoint.size(); i++)
          errs()<<"CheckPoint:"<<*CheckPoint[i]<<"\n";
       //tolerance
-      BasicBlock::iterator ignoreuntilinst;
 
+      BasicBlock::iterator ignoreuntilinst;
+      /*AllocaInst* recovery_int;
+      AllocaInst* recovery_fp;
+      bool first_int=false;
+      bool first_fp=false;*/
       for (auto &B : F) {
         //errs() << "Basic block:\n";
         //B.dump();
         bool BuilderAfterflag=1;
         for (auto &I : B) {
+               
+                
         //BasicBlock* bb=I.getParent();
         //Find allocation instruction
             //I.dump();
@@ -263,16 +272,24 @@ namespace {
             //errs()<<*scalar_t<<"\n";
             //errs()<<*scalar_t1<<"\n";
             //support inst type?
+            
             if(scalar_t->isIntegerTy()){
                 
                 //errs()<<"Find integer:"<<*scalar_t<<"\n";
-                auto allocaVec = builder.CreateAlloca(VectorType::get(scalar_t, 4),nullptr,"allocaVec");
+	        	auto allocaVec = builder.CreateAlloca(VectorType::get(scalar_t, 4),nullptr,"allocaVec");
                 allocaVec->setAlignment(16);
                 //errs()<<"address sca:"<<*op<<",vec:"<<*allocaVec<<"\n";
                 vec_map.AddPair(op,allocaVec);
                 //get vector demo
                 auto *vec = vec_map.GetVector(op);
                 errs()<< "Tolerance:Create AllocaInst Vectorty:"<<*vec<<"\n";
+                /*if(!first_int)
+                {
+                //Type::getFloatTy(TheContext);
+                recovery_int=builder.CreateAlloca(scalar_t,nullptr,"Recovery");
+                recovery_int->setAlignment(4);
+                first_int=true;
+                }*/
                 //errs()<<"get vector:"<<*vec<<"\n";
             }  
             if(scalar_t->isFloatTy()){
@@ -283,6 +300,13 @@ namespace {
                 //get vector demo
                 auto *vec = vec_map.GetVector(op);
                 errs()<< "Tolerance:Create FloatInst Vectorty:"<<*vec<<"\n";
+                /*if(!first_fp)
+                {
+                //Type::getFloatTy(TheContext);
+                recovery_fp=builder.CreateAlloca(scalar_t,nullptr,"Recovery");
+                recovery_fp->setAlignment(4);
+                first_fp=true;
+                }*/
             }
             if(scalar_t->isVectorTy()){
                 //errs()<<"Find vector:"<<*scalar_t<<"\n";
@@ -331,8 +355,8 @@ namespace {
                 {
                     //errs()<<"*****load_val:"<<*load_val<<"\n";
                     //errs()<<"******:"<<*loadinst_ptr<<"\n";
-                    //create insertelement instruction
-                    val = builderafter.CreateInsertElement(val,op, builderafter.getInt32(i),"insertElmt");  
+           			//create insertelement instruction
+    				val = builderafter.CreateInsertElement(val,op, builderafter.getInt32(i),"insertElmt");  
                 }
                 //get vector in map
                 auto *vec = vec_map.GetVector(loadinst_ptr);
@@ -356,9 +380,9 @@ namespace {
             // Insert at the point where the instruction `op` appears.
             IRBuilder<> builder(op);
             Value* lhs = op->getOperand(0);
-            Value* rhs = op->getOperand(1);
+	    	Value* rhs = op->getOperand(1);
             //errs()<<"lhs:"<<*lhs<<"\n";
-            //errs()<<"rhs:"<<*rhs<<"\n";
+	    	//errs()<<"rhs:"<<*rhs<<"\n";
             //Value *mul_value = ConstantInt::get(op->getType() , 3); 
             //Value* mul = builder.CreateMul(lhs, mul_value);
             Value* load_val1=GetVecOpValue(builder,lhs,vec_map);
@@ -366,12 +390,12 @@ namespace {
             Value *vop;
             //#1
             if(strcmp(op_name, "add") == 0){// find op is "add"
-            //errs()<<"Find add:"<<op_name<<"\n";
+		    //errs()<<"Find add:"<<op_name<<"\n";
                     if(load_val1!=NULL&&load_val2!=NULL) {
                     vop = builder.CreateAdd(load_val1,load_val2,"Vop");
                     //errs()<<"Create add:"<<*vadd<<"\n";
                     //map add
-                    vec_map.AddPair(op, vop);
+ 	                vec_map.AddPair(op, vop);
                     }else{
                     errs()<<"Error: in Vector operator:"<<*op<<"\n";
                     }
@@ -452,8 +476,8 @@ namespace {
                 errs()<<*op<<" - Find Check Point:"<<*user<<"\n";
                 Value *rdst = user->getOperand(1);
                 auto *vecdst  = vec_map.GetVector(rdst);
-                errs()<<"XXXXXXXXXXXxVOPd:"<<*vop<<"\n";
-                errs()<<"XXXXXXXXXXXxFind:"<<*vecdst<<"\n";
+                //errs()<<"XXXXXXXXXXXxVOPd:"<<*vop<<"\n";
+                //errs()<<"XXXXXXXXXXXxFind:"<<*vecdst<<"\n";
                 Value *vecstr = builder.CreateStore(vop,vecdst);
                 vec_stored_map.AddPair(rdst,vecdst);//save vec have stored map
                 if(isa<StoreInst>(*user)){//Find final store 
@@ -519,20 +543,27 @@ namespace {
                 AllocaInst* recovery=builder.CreateAlloca(recoveryinst->getType(),nullptr,"Recovery");
                 recovery->setAlignment(4);
                 errs()<<"Recovery:"<<*recovery<<"\n";
-                Value *Rval = builderafter.CreateStore(op, recovery);
-
+                //errs()<<"XXXXXXXXXXX1:\n";
+                //errs()<<"XXXXXXXXXXXxOP:"<<*op<<"\n";
+                //errs()<<"XXXXXXXXXrecovery:"<<*recovery_fp<<"\n";
+                Value *Rval = builder.CreateStore(op, recovery);
+                //errs()<<"XXXXXXXXXXX2:\n";
                 //#3
                 Value *vadd,*vadd1,*fault_check;
                 if(op_type->isIntegerTy()){
                 vadd = builder.CreateAdd(ex0,ex1,"sum");
                 vadd1 = builder.CreateAdd(vadd ,ex2,"sum");
                 fault_check=builderafter.CreateICmpNE(vadd1,mul,"Fcmp");
+                //Value *Rval = builder.CreateStore(op, recovery_int);
+                //recovery_map1.AddPair(user,recovery_int);
                 //errs()<<"add"<<*vadd<<"\n";
                 }else if(op_type->isFloatTy()){
                 vadd = builder.CreateFAdd(ex0,ex1,"sum");
                 vadd1 = builder.CreateFAdd(vadd ,ex2,"sum");
                 //errs()<<"Fadd"<<*vadd<<"\n";
                 fault_check=builderafter.CreateFCmpUNE(vadd1,mul,"Fcmp");
+                //Value *Rval = builder.CreateStore(op, recovery_fp);
+                //recovery_map1.AddPair(user,recovery_fp);
                 }else {
                 errs()<<"#3 Not Support Operator Type:"<<*op_type<<"\n";
                 }
@@ -544,6 +575,7 @@ namespace {
                 check_map.AddPair(fault_check,ex0);
                 recovery_map.AddPair(user,op);
                 recovery_map1.AddPair(user,recovery);
+
                 //TerminatorInst *ThenTerm = nullptr, *ElseTerm = nullptr;
                 //BasicBlock::iterator it(op1);
 
@@ -686,6 +718,7 @@ namespace {
             }
       }
       //replace recovery value to store
+      
      for (auto &B : F) {
                 //B.dump();
                 for (auto &I : B) {
@@ -702,14 +735,14 @@ namespace {
                 }
             }
       errs()<<"Show LLVM IR:\n";
-      /*
+      
       for (auto &B : F) {
             B.dump();
             for (auto &I : B) {
               //I.dump();
           }
         }
-        */
+        
       errs()<<"Vector Map:\n";
       PrintMap(&vec_map);
       errs()<<"Check Map:\n";
